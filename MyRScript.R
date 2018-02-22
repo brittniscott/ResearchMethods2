512ef3d4a8221687514d0f8ebf28d68c23bbe173
Philip Taylor <philip.taylor@acadiau.ca>
2018-02-22 15:46:30 -0400

    I made a few small changes to the axis labels. Nice work!

--- MyRScript.R
+++ MyRScript.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(lubridate)
 
+## Nice work ... Phil.
+
+blpw.all <- readRDS("blpw.all.RDS")
+
 # STEP 1: Remove rows with only one mass value per band number (removes birds that were not recaptured and birds that were recaptured but with only one observation present)
 blpw.all <- blpw.all %>% 
   group_by(band) %>% 
@@ -28,6 +32,8 @@ ggplot(data = blpw.all, mapping = aes(x = (yday(date)), y = cumdelta, colour = b
   facet_wrap(~ location) +
   ylab("Mass (in grams, relative to capture date)") +
   xlab("Time of Year") +
-  scale_x_continuous(breaks = c(213, 244, 274, 305), labels = function(x) format(as.Date(as.character(x), "%j"), "%b"))
+  scale_x_continuous(breaks = c(213, 227, 244, 258, 274, 288), 
+                     labels = function(x) format(as.Date(as.character(x), "%j"), "%d-%b")) + 
+  theme(axis.text.x=element_text(angle=90,hjust=1))
 
   
