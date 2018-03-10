b4674ac93d142d3ab6b9367c37aa65548f0b7274
brittniscott <brittniscott@acadiau.ca>
2018-03-10 15:06:54 -0400

    Linear stuff worked through

--- Stats Assignment.R
+++ Stats Assignment.R
@@ -1,17 +1,17 @@
 library(tidyverse)
 
 
-# QUESTION 1
+# QUESTION 1-3
 
 
-# This is the function we will use to geneterate a data frame for x and y values
+# This is the function we will use to geneterate a dataframe for x and y values
 gen.df <- function(n = 100, b0 = 2, b1 = 3, sd.err = 1)  {
   predictor <- seq(-1, 1, length.out = n)
   response <- b0 + b1 * predictor + rnorm(n, mean = 0, sd = sd.err)
   return(data.frame(response = response, predictor = predictor))
 }
 
-# We then need a dataframe conatining the various values of the parameters we wish to apply to our function
+# We then need a dataframe containing the various values of the parameters we wish to apply to our function
 n.times <- 1000
 rep <- 1:n.times
 n <- c(10, 100, 1000)
@@ -51,3 +51,36 @@ test.sum <-
 
 p <- ggplot(data = test.sum, aes(b1, gm, col = n, group = n))
 p + geom_point() + geom_line() + facet_grid(sd.err ~ .) + scale_y_log10()
+
+
+
+# QUESTION 4
+
+
+gen.df4 <- function(n = 100, b0 = 0, b1 = 0, sd.err = 1)  {
+  predictor <- seq(-1, 1, length.out = n)
+  response <- b0 + b1 * predictor + rnorm(n, mean = 0, sd = sd.err)
+  return(data.frame(response = response, predictor = predictor))
+}
+
+n.times <- 5000
+rep <- 1:n.times
+n <- c(10, 100, 1000)
+sd.err <- c(0.1, 1, 2, 5)
+test.df4 <- expand.grid(n.times = n.times, n = n, rep = rep, sd.err = sd.err)
+
+
+for (i in 1:length(test.df4$rep)) {
+  tmp.df4 <- gen.df4(sd.err = test.df4$sd.err[i],
+                     n = test.df4$n[i])
+  tmp.lm4 <- lm(response ~ predictor, data = tmp.df4)
+  test.df4$pval[i] <- summary(tmp.lm4)$coefficients["predictor", "Pr(>|t|)"]
+}
+
+test.df4$sd.err <- factor(test.df4$sd.err)
+
+p <- ggplot(data = test.df4, aes(pval))
+p + geom_density() + facet_grid(n ~ sd.err)
+
+group_by(test.df4, sd.err, n) %>% 
+  summarise(sum.tab = table(pval <= 0.05)[2]/length(pval))
